22bcea3bfa1b42e2627541913ca9998ff09fffa1
renan-augusto3850 <122371800+renan-augusto3850@users.noreply.github.com>
2023-01-11 10:28:09 -0300

    install.sh

--- instalation.sh
+++ instalation.sh
@@ -1,9 +1,9 @@
 sudo apt-get update
 
-sudo apt-get install python3-pip
+sudo apt install python3
 
-sudo apt-get install python3
+sudo apt install python3-pip
+
+sudo apt install python-tk python3-tk
 
 pip install PySimpleGUI
-
-sudo apt-get install python-tk python3-tk
